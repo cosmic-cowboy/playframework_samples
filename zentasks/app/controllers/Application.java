8af149946474840c37243503e6f6b2af886bbe28
hiroo takaku <slgerkamp@gmail.com>
2014-05-15 22:28:22 +0900

    ログインスクリーンの実装

--- zentasks/app/controllers/Application.java
+++ zentasks/app/controllers/Application.java
@@ -2,17 +2,64 @@ package controllers;
 
 import models.Project;
 import models.Task;
+import models.User;
 import play.*;
+import play.data.Form;
 import play.mvc.*;
 import views.html.*;
+import views.html.helper.form;
 
 public class Application extends Controller {
 
+	
+    /**************
+     * ログイン画面　*
+     **************/
+	/**
+	 * ログイン用フォーム
+	 */
+	public static class Login {
+		public String email;
+		public String password;
+		
+		public String validate(){
+			if(User.authenticate(email, password) == null){
+				return "メールアドレス、もしくは、パスワードが正しくありません";
+			}
+			return null;
+		}
+	}
+	
+	/**
+     * ログイン画面のアクション
+     * @return
+     */
+    public static Result login() {
+    	Form<Login> f = new Form(Login.class);
+        return ok(login.render(f));
+    }
+    
+    public static Result authenticate(){
+    	Form<Login> loginForm = new Form(Login.class).bindFromRequest();
+    	if(loginForm.hasErrors()){
+    		return badRequest(login.render(loginForm));
+    	} else {
+    		session().clear();
+    		session("email", loginForm.get().email);
+    		return redirect(routes.Application.index());
+    	}
+    }
+    /**************
+     * トップページ　*
+     **************/
+    /**
+     * トップページのアクション
+     * @return
+     */
     public static Result index() {
         return ok(index.render(
         		Project.find.all(),
         		Task.find.all()
         ));
     }
-
 }
